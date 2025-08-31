cdf7816768862a4a31129bbdc66d95a3a55e85dd
Carl Mathew Replan <mattreplan@gmail.com>
2025-08-31 10:20:19 +0800

    Added Link to Reserve a Table button

--- src/Component/Hero.js
+++ src/Component/Hero.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function Hero() {
   return (
     <section id="hero" className="hero container">
@@ -6,7 +8,9 @@ export default function Hero() {
         <h2>Chicago</h2>
         <p>We are a family owned Mediterranean restaurant,
           <br /> focused on traditional recipes served with a modern twist.</p>
+        <Link to="/booking">
         <button>Reserve a Table</button>
+        </Link>
       </div>
       <img src="/images/icons_assets/restauranfood.jpg" alt="Little Lemon restaurant" />
     </section>
