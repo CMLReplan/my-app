f8a6f4fa80fd4c6f9d7ad3d5fd1c5896fb804ad3
Carl Mathew Replan <mattreplan@gmail.com>
2025-09-01 11:59:28 +0800

    Added props, initialize and update times

--- src/Main.js
+++ src/Main.js
@@ -1,14 +1,41 @@
+import React, { useReducer } from "react";
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./Pages/Homepage";
 import BookingPage from "./Pages/BookingPage";
 
-function Main() {
-  return (
-    <Routes>
-      <Route path="/" element={<Homepage />} />
-      <Route path="/booking" element={<BookingPage />} />
+// Function to initialize available times
+const initializeTimes = () => {
+  return ['17:00', '18:00', '19:00', '20:00', '21:00'];
+};
 
-    </Routes>
+// Reducer function to update times
+const updateTimes = (state, action) => {
+  if (action.type === 'UPDATE_TIMES') {
+    // For now, return the same times regardless of date
+    return ['17:00', '18:00', '19:00', '20:00', '21:00'];
+  }
+  return state;
+};
+
+
+function Main() {
+  const [availableTimes, dispatch] = useReducer(updateTimes, [], initializeTimes);
+
+  return (
+    <main>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route
+          path="/booking"
+          element={
+            <BookingPage
+              availableTimes={availableTimes}
+              dispatch={dispatch}
+            />
+          }
+        />
+      </Routes>
+    </main>
   );
 }
 
